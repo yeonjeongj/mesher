acc5153880a994198837c921a8f430dfedfd04c1
yeonjeongj <91460226+yeonjeongj@users.noreply.github.com>
2023-03-01 13:36:21 +0900

    Add: Finish style setting & basic UI (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Router from "./Router";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+
+import Router from './Router';
+import GlobalStyle from './styles/GlobalStyles';
+import theme from './styles/theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+const theme: { [key: string]: string } = {
+  bgColor: '#1a1c1f',
+  bgLight: '#212328',
+};
+
+export default theme;
